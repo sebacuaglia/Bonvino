955234f4caafd7db278f8247dad013a7b38b4684
sebac <sebac@DESKTOP-P36GHUK>
2024-05-11 01:51:50 -0300

    inicio

---
+++ bonvino/src/main/java/ar/com/bonvino/BonvinoApplication.java
@@ -0,0 +1,13 @@
+package ar.com.bonvino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BonvinoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BonvinoApplication.class, args);
+	}
+
+}
---
+++ bonvino/src/test/java/ar/com/bonvino/BonvinoApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.com.bonvino;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BonvinoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
